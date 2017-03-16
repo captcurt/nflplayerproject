64634cc1cfafff8274f348ce19cd7aa7d5ab4f94
captcurt <JCurtner1@my.gcu.edu>
2017-03-15 21:16:47 -0500

    Add files via upload

---
+++ NFLPLAYERS.java
@@ -0,0 +1,30 @@
+import javax.swing.Box;
+
+public abstract class NFLPLAYERS {
+	
+	int nflplayer1 ;
+	String nflplayer name;
+	double player1tackles;
+	double player1interceptions;
+	double player1sacks;
+	double player1fubles;
+	
+	void caculateplayerstates(Object playertotalstat, Object playertackles){
+	    Object playerinterceptions;
+		Object playersacks;
+		Object playerfumbles;
+		playertotalstat = playerinterceptions + playersacks+ playerfumbles;{
+			
+		}
+	
+		
+	
+		 
+			
+		
+	}
+	
+	
+}
+  
+ 
